75fe08f2c606f3a21cdb761e48399354fb65542b
LonelySushii <lenhilam1234@gmail.com>
2023-09-11 16:34:34 +1000

    Intialised project

---
+++ Test Ruby/Assets/2DBeginnerTutorialResources/Art/Sprites/Scripts/damagezone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagezone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        rubycontroller controller = other.GetComponent<rubycontroller>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
